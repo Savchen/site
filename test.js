f2d7b6c393f9c46625fd4bdaa60c8b96da533a12
Savchen <38027103+Savchen@users.noreply.github.com>
2018-04-03 14:07:40 +0900

    add
    add

---
+++ test.js
@@ -0,0 +1,21 @@
+jQuery('document').ready(function() {
+
+   jQuery('button').on('click', function() {
+
+    var value1 ,value2 , value3;
+
+    value1 = jQuery('#vas1').val();
+    value2 = jQuery('#vas2').val();
+
+    value1 = parseInt(value1);
+    value2 = parseInt(value2);
+
+
+    value3 = value1 * value2;
+
+    alert(value3);
+
+
+  });
+
+});
